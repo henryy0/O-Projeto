c23f44c03278883dbe0d520082f88264bfd8da8e
henryy0 <129812808+henryy0@users.noreply.github.com>
2023-11-01 15:05:49 -0300

    Add files via upload

---
+++ O Projeto/O-Projeto/O Projeto/Evento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace O_Projeto
+{
+    public partial class Evento : Form
+    {
+        string stringConexao = "Data Source=localhost;Initial Catalog=t18_estoque;User ID=sa;Password=123456";
+        public Evento()
+        {
+            InitializeComponent();
+        }
+
+        private void Evento_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
